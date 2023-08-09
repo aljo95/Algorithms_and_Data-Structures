5066dbacbdd87e86a45109bf7499eef6b2e6d2a2
aljo <johansson_a95@hotmail.com>
2023-08-10 00:38:18 +0200

    longest substring java version

---
+++ Other/LongestSubstring.java
@@ -0,0 +1,29 @@
+public class LongestSubstring {
+    public static void main(String[] args) {
+
+        String exampleString = "abcabcbb";
+
+        int result = lengthOfLongestSubstring(exampleString);
+        System.out.println(result);
+
+    }
+    public static int lengthOfLongestSubstring(String s) {
+
+        char[] sChars = new char[s.length()];
+        int count = 0;
+
+        for (int i=0; i<s.length(); i++) {
+
+            if (i > 0) {
+                for (char j: sChars) {
+                    if (j == s.charAt(i)) {
+                        return count;
+                    }
+                }
+            }
+            sChars[i] = s.charAt(i);
+            count++;
+        }
+        return count;
+    }
+}
