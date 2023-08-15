16383589834e550c46611aa0139692fe1da6918e
aljo <johansson_a95@hotmail.com>
2023-08-15 11:58:23 +0200

    refactor

--- Other/ReverseInteger.java
+++ Other/ReverseInteger.java
@@ -10,18 +10,15 @@ public class ReverseInteger {
 
         String reverseString = "";
         String numberLength = "" + x;
-       // int 
         int lastNumber;
 
         while (x != 0) {
             lastNumber = x % 10;
-            reverseString += "" + lastNumber;   //"3", "32", "321"
-            numberLength = numberLength.substring(0, numberLength.length() -1); //"123" -> "12" -> "1"
+            reverseString += "" + lastNumber;
+            numberLength = numberLength.substring(0, numberLength.length() -1);
             if (numberLength == "") return Integer.parseInt(reverseString);
             x = Integer.parseInt(numberLength);
         }
         return 0;
-        //return Integer.parseInt(reverseString);
-
     }
 }
