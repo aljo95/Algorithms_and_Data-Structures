67483ca61546d5c6c4d78cfb9c4a36566235163f
aljo <johansson_a95@hotmail.com>
2023-08-16 21:23:57 +0200

    Water Container improved algorithm

---
+++ Other/WaterContainerImproved.java
@@ -0,0 +1,32 @@
+
+public class WaterContainerImproved {
+
+    public static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        //int [] height = {1, 1};
+        System.out.println(maxArea(height));
+    }
+
+    public static int maxArea(int[] height) {
+
+        int l = 0;
+        int r = height.length - 1;
+        int max = 0;
+
+        while (l < r) {     
+            if (height[l] > height[r]) {
+                max = Math.max(max, height[r] * (r-l));
+                r--;
+            } else {
+                max = Math.max(max, height[l] * (r-l));
+                l++;
+            }
+
+            System.out.println(max);
+        }
+
+        return max;
+    }
+}
+            //1: l=0, r=8,  =>   max=h[l] * r-l = 1 * (8-0) = 8
+            //2: l=1, r=8   =>   max=h[r] * r-l = 7 * (8-1) = 7*7=49
