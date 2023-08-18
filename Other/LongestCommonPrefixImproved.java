5720b96efc67df71f124326ed4492623922135e4
aljo <johansson_a95@hotmail.com>
2023-08-18 23:18:49 +0200

    fix: condition

--- Other/LongestCommonPrefixImproved.java
+++ Other/LongestCommonPrefixImproved.java
@@ -8,20 +8,22 @@ public class LongestCommonPrefixImproved {
     }   
     public static String longestCommonPrefix(String[] strs) {
         
-        Arrays.sort(strs);  //strings sorted by alphabetical order -> can just compare the first and last one
+        //strings sorted by alphabetical order: ["flight","flow","flower"]
+        Arrays.sort(strs);
 
         String first = strs[0];
         String last = strs[strs.length - 1];
 
         int index = 0;
-        while(index < first.length()) {
+        while(index < first.length() && index < last.length()) {
+
+            //because strings are sorted alphabetically we can just compare the first and last string elements
             if (first.charAt(index) == last.charAt(index)) {
                 index++;
             } else {
                 break;
             }
         }
-
         return first.substring(0, index);
     }
 }
