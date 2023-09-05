09bf5bac97ac578b3a9e3faa59a6c02c502f1895
aljo <johansson_a95@hotmail.com>
2023-09-05 23:26:56 +0200

    refactor and package

--- Sorting-and-Searching/Linear.java
+++ Sorting-and-Searching/Linear.java
@@ -1,14 +1,13 @@
+package com.mycompany.sortandsearch;
+
 public class Linear {
 
     static void search(int[] array, int i) {
-        
-        
-        int j = 0;
-        for (j=0; j<array.length; j++) {
+
+        for (int j=0; j<array.length; j++) {
            if (array[j] == i) {
                 System.out.println("Linear search found key: " + i + " at index " + j + ".");
             }
         }
     }
-    
 }
