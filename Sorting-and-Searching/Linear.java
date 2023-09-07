b2414885e662db829e233a59bd96337541930007
aljo <johansson_a95@hotmail.com>
2023-09-07 23:39:48 +0200

    optimization added

--- Sorting-and-Searching/Linear.java
+++ Sorting-and-Searching/Linear.java
@@ -2,12 +2,14 @@ package com.mycompany.sortandsearch;
 
 public class Linear {
 
-    static void search(int[] array, int i) {
-
+    static boolean search(int[] array, int i) {
+        
+        if (array[array.length-1] < i) return false;
+        
         for (int j=0; j<array.length; j++) {
-           if (array[j] == i) {
-                System.out.println("Linear search found key: " + i + " at index " + j + ".");
-            }
+           if (array[j] > i) return false;
+           else if (array[j] == i) return true;
         }
+        return false;
     }
 }
