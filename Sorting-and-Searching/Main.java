795fd58ddc3ee46a57b567e9326ed084f535eb82
aljo <johansson_a95@hotmail.com>
2023-09-09 22:39:28 +0200

    Final version

--- Sorting-and-Searching/Main.java
+++ Sorting-and-Searching/Main.java
@@ -1,24 +1,21 @@
+
 package com.mycompany.sortandsearch;
 
 import java.util.Random;
 
 /**
- * @author Alexander Johansson
+ * @author 
  */
-
 public class Main {
-    
-    //denna function kallas 1000 gånger (k=1000), och kör 10000 gånger varje gång. 1000*10000
     
     //array = 200 random sorted values, indx = 10000 random values unsorted (0-999)
     
     private static void linear(int[] array, int[] indx) {
         boolean linearResult;
         for (int i=0; i<indx.length; i++) {
-            linearResult = Linear.search(array, indx[i]);       //Linear.search går n=200 gånger för varje key
-        }                                                   //k=1000, loop=10000, n=200
+            linearResult = Linear.search(array, indx[i]);       
+        }                                                   
     }
-    
     
     private static void binary(int[] array, int[] indx) {
         boolean binaryResult;
@@ -26,16 +23,6 @@ public class Main {
 	    binaryResult = Binary.search(array, indx[i]);
 	}
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     private static int[] sorted(int n) {    // n = 100 first time
 	Random rnd = new Random();	
@@ -49,7 +36,6 @@ public class Main {
 	return array;
     }
        
-    //loop=10000 or (10), n=100 first time
     private static int[] keys(int loop, int n) {
 	Random rnd = new Random();	
 	int[] indx = new int[loop];
@@ -59,8 +45,6 @@ public class Main {
 	return indx;
     }
     
-    
-    //unsorted with only unique values
     private static int[] unsorted(int n) { 
         
         int[] unsortedArray = new int [n];
@@ -78,28 +62,12 @@ public class Main {
         return unsortedArray;
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     private static void linearUnsorted(int[] array, int[] indx) {
         boolean res;
         for (int i=0; i<indx.length; i++) {       //0 - 9999, 10000 times
             res = search_unsorted(array, indx[i]);
         }
     }
-    
-    
-    
     
     public static boolean search_unsorted(int[] array, int key) {
         for (int index = 0; index < array.length ; index++) {
@@ -111,22 +79,6 @@ public class Main {
     }
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
     public static void main(String[] args) {
         
         int[] sizes = {100 ,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500,1600};
@@ -143,13 +95,12 @@ public class Main {
             int [] unsortedArray = unsorted(n); //generate unsorted array of size n
             /* */
 
-	    System.out.printf("\n%8d", n);    //100
+	    System.out.printf("\n%8d", n);    
 
 	    int k = 1000;
 	    
-	    double min = Double.POSITIVE_INFINITY;
             /******************UNSORTED****************************************/
-            
+            double min = Double.POSITIVE_INFINITY;
             
             for (int i = 0; i < k; i++) {
 		long t0 = System.nanoTime();
@@ -165,30 +116,20 @@ public class Main {
             
             /**********************LINEAR************************************/
             
-            
-            
-            
-            
             min = Double.POSITIVE_INFINITY;
 
 	    for (int i = 0; i < k; i++) {           
 		long t0 = System.nanoTime();
-		linear(array, indx);            //10000 sökningar görs och tiden sparas. Detta görs 1000 gånger (k) och de snabbaste 10000 sökningarna sparas i min
+		linear(array, indx);            
 		long t1 = System.nanoTime();
 		double t = (t1 - t0);
-		if (t < min)                        //min will be updated with shortest value each time
+		if (t < min) //min will be updated with shortest value each time
 		    min = t;
 	    }
             //System.out.println("nano time: " + min);
-	    System.out.printf("%8.0f", (min/loop));	//tid för en sökning.    ex: 451800ns = [en sökning] * 1000. En sökning = 451800 / 10000
-                                                                                         //     [en sökning] = 451800 / 10000 = ca 45 ns ?
-
-            
-            
+	    System.out.printf("%8.0f", (min/loop));	
+              
             /*************************BINARY*********************************/
-            
-            
-            
             
 	    min = Double.POSITIVE_INFINITY;
 	    
@@ -203,7 +144,6 @@ public class Main {
 
             //System.out.println("nano time: " + min);
 	    System.out.printf("%8.0f" , (min/loop));
-            
 	}
     }
 }
