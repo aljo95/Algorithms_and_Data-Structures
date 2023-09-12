d8fc248df6704be204521cbc25b8fa3447cd9bea
aljo <johansson_a95@hotmail.com>
2023-09-12 23:19:51 +0200

    final version

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,40 +1,21 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 public class InsertionSort {
 
-
-    public static int[] insertionSort(int[] array) {    // arr[] = {64, 25, 12, 22, 11} 
-                                                        // arr[] = {25, 64, 12, 22, 11} 
-        for (int i=1; i<array.length; i++) {          // arr[] = {25, 12, 64, 22, 11} 
-                                                        // arr[] = {25, 64, 12, 22, 11}  
-            for (int j=i-1; j>=0; j--) {
-
-                if (array[j+1] < array[j]) {
-                    int tmp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = tmp;
-                }
-
+    public static int[] insertionSort(int[] array) {  
+                                                        
+        for (int i=0; i<array.length; i++) {            
+            for (int j=i; j>0 && (array[j] < array[j-1]) ; j--) {
+                int tmp = array[j];
+                array[j] = array[j-1];
+                array[j-1] = tmp;
             }
         }
-
         return array;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     private static void printArray(int arr[])
     {
@@ -46,54 +27,45 @@ public class InsertionSort {
 
 
     // Generate UNIQUE random numbers for array of length n. Numbers are in range 0 to n*10 (excluded)
-    private static int[] unsorted(int n) {
-        int[] unsortedArray = new int [n];
-        Random rnd = new Random();
-
-        for (int i=0; i<unsortedArray.length; i++) {
-            unsortedArray[i] = rnd.nextInt(n*10);
-            for (int j=0; j<i; j++) {
-                if (unsortedArray[i] == unsortedArray[j]) {
-                    unsortedArray[i] = rnd.nextInt(n*10);
-                    i--;
-                }
-            }
+    private static int[] unsorted(int n) { 
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i=0; i<n; i++) list.add(i);
+        Collections.shuffle(list);
+        int[] randomArray = new int[n];
+        for (int i=0; i<n; i++) {
+            randomArray[i] = list.get(i);
         }
-        return unsortedArray;
+        return randomArray;
     }
 
-
+    // Generate unique sorted numbers
+    private static int[] sorted(int n) {
+        Random rnd = new Random();
+        int[] array = new int[n];
+        int nxt = 0;
+        for (int i = 0; i < n ; i++) {
+            nxt += rnd.nextInt(10) + 1;
+            array[i] = nxt;
+        }
+        return array;
+    }
 
     public static void main(String[] args) {
-/* 
-        int[] arr = unsorted(10);
-        printArray(arr);
-        int[] sorted = insertionSort(arr);
-        printArray(sorted);
-*/
-
         
         double result = 0;
-        for (int i=100; i<=1000; i+=100) {
+        for (int i = 100; i <= 1600; i += 100) {
             double min = Double.MAX_VALUE;
             
             for (int j=0; j<10000; j++) {
-                int[] arr = unsorted(i);    //gives array of length 100, 200, 300 ..., 1000
-                            //prints the return
+                int[] arr = unsorted(i); 
+                //int[] arr = sorted(i); 
                 long t0 = System.nanoTime();
-                //printArray(selectionSort(arr));  //calls insertions sort with the array(s) prints
                 insertionSort(arr);
                 long t1 = System.nanoTime();
                 result = (t1-t0);
                 if (result < min) min = result;
             }
-            System.out.println(i + ": " + result);
-    
-        } 
-
-
-
-
-
+            System.out.println(i + " " + min);
+        }
     }
 }
