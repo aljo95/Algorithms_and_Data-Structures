ae3fbc71738934f397e8333cebe29e6cb37db46c
aljo <johansson_a95@hotmail.com>
2023-09-15 23:29:48 +0200

    final merge sort

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -8,17 +8,18 @@ public class MergeSort {
         int[] aux = new int[org.length];
         sort(org, aux, 0, org.length-1);
     }
-
+    
     public static void sort(int[] org, int[]aux, int lo, int hi) {
 
-        if (lo != hi) {  
+        if (lo != hi) {                       
             int mid = (lo+hi)/2;    
 
-            sort(org, aux, lo, mid);    
-            sort(org, aux, mid+1, hi); 
+            //sort first and second halves    
+            sort(org, aux, lo, mid);            
+            sort(org, aux, mid+1, hi);         
 
-            //merge                          
-            merge(org, aux, lo, mid, hi); 
+            //merge                            
+            merge(org, aux, lo, mid, hi);     
         }
     }
 
@@ -29,34 +30,35 @@ public class MergeSort {
         for (int i=0; i<org.length; i++) {
             aux[i] = org[i];
         }
-        
-        int i = lo;              
-        int j = mid+1;     
 
-        for (int k=lo; k<=hi; k++) {    
+        // merging time
+        int i = lo;         //index first part                  
+        int j = mid+1;      //index second part
+
+        //for all indices from lo to hi       
+        for (int k=lo; k<=hi; k++) {     
             if (i > mid) {
-                org[k] = aux[j];            
-                j++;       
+                org[k] = aux[j];                  
+                j++;                             
             }
-            else if (j > hi) {    
+            else if (j > hi) {                     
                 org[k] = aux[i];
                 i++;
             } 
-            else if (aux[i] < aux[j]) {             
-                org[k] = aux[i];    
+            else if (aux[i] < aux[j]) {    
+                org[k] = aux[i];                 
                 i++;
             }
-            else if (aux[i] > aux[j]){  
+            else if (aux[i] > aux[j]){        
                 org[k] = aux[j];
                 j++;
             }
-        }                                          
+        }                                           
 
     }                                               
 
     // Generate UNIQUE random numbers for array of length n. Numbers are in range 0 to n*10 (excluded)
     private static int[] unsorted(int n) { 
-
         ArrayList<Integer> list = new ArrayList<Integer>();
         for (int i=0; i<n; i++) list.add(i);
         Collections.shuffle(list);
@@ -68,7 +70,6 @@ public class MergeSort {
     }
 
     private static void printArray(int arr[]) {
-
         int n = arr.length;
         for (int i=0; i<n; ++i)
             System.out.print(arr[i]+" ");
@@ -76,6 +77,13 @@ public class MergeSort {
     }
 
     public static void main(String[] args) {
+
+        /* 
+        int[] arr = unsorted(100);
+        printArray(arr);
+        sort(arr);
+        printArray(arr);
+        */
         
         double result = 0;
         for (int i = 100; i <= 1600; i += 100) {
