53c428e734a8a2abb6ef87e3bfb21508b03a1c83
aljo <johansson_a95@hotmail.com>
2023-10-01 23:19:49 +0200

    Queue for BST nodes

--- Trees/Binary Tree/Queue Iterator/Queue.java
+++ Trees/Binary Tree/Queue Iterator/Queue.java
@@ -7,7 +7,6 @@ public class Queue {
     
     Node head;
 
-    
     public class Node {
         
         public Node next;
@@ -19,33 +18,20 @@ public class Queue {
             this.next = null;
         }
     }
-        
     
-    public Queue() {
-        
-    }
-    
-    //add 
     public void add(BinaryTree.Node item) {
         
-        
-        
-        
         if (this.head == null){
-            this.head = new Node(item, null);       // 1> 2> null
+            this.head = new Node(item, null);
             return;
         }
         
         Node currentNode = this.head;
         while(currentNode.next != null)  {
-            
             currentNode = currentNode.next;
         }
         currentNode.next = new Node(item, null);
     }
-    
-    
-    
     
     public Node remove() {
         if (this.head == null) {
@@ -58,23 +44,17 @@ public class Queue {
         removedNode.next = null;            // necessary?
         return removedNode;
     }
-    
-    
-    
-        //LENGTH METHOD FOR CHECKING
+
     public int length() {
         if (this.head == null)
             return 0;
         
         int count = 1;
         Node next = this.head;
-        while (next.next != null) {     // 1> 2> 3>null
+        while (next.next != null) { 
             count++;
             next = next.next;
         }
         return count;
     }
-    
-    
 }
-
