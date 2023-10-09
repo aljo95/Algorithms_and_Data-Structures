808e0fcad8daaae9127ae3b2ff5a7dcc19c71f64
aljo <johansson_a95@hotmail.com>
2023-10-09 23:49:04 +0200

    better version, one line

--- Other/LengthOfArguments.js
+++ Other/LengthOfArguments.js
@@ -1,26 +1,12 @@
+
 /**
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
-/*
-var argumentsLength = function(...args) {
 
-    let count = 0;
-    for (let i=0; i<args['0'].length; i++) {
-        count++;
-    }
-    return count;
-};
-*/
+let argumentsLength = (...args) => args['0'].length;
 
-let argumentsLength2 = (...args) => args['0'].length;
+let arguments = [{}, null, "3", 1, true, [1, 2]];
 
-
-args1 = [{}, null, "3", 1, true, [1, 2]];
-console.log(argumentsLength2(args1));
-
-
-
-/**
- * argumentsLength(1, 2, 3); // 3
- */
+// returns 6, because 6 arguments
+console.log(argumentsLength(arguments));
