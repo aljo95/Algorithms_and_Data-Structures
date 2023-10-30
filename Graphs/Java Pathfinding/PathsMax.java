39cc019dc47a897d3781f9269e69e946ff46d82c
aljo <johansson_a95@hotmail.com>
2023-10-30 22:40:47 +0100

    Even better version than Paths

---
+++ Graphs/Java Pathfinding/PathsMax.java
@@ -0,0 +1,69 @@
+
+public class PathsMax {
+    
+    City[] path;
+    int sp;
+    
+    public PathsMax() {
+        path = new City[54];
+        sp = 0;
+    }
+    
+    public Integer shortest(City from, City to) {
+        
+        
+        if (from == to) {
+            return 0;
+        }
+        Integer max = null;
+        Integer shrt = null;
+        
+        for (int i = 0; i < sp; i++) {
+            if (path[i] == from)
+            return null;
+        }
+
+        
+        
+        path[sp++] = from;
+        for (int i = 0; i < from.neighbours.length; i++) {
+            if (from.neighbours[i] != null) {
+                
+                Connection conn = from.neighbours[i];
+                
+                if (max != null && conn.distance > max)
+                    continue;
+                
+                
+                Integer temp = shortest(conn.city, to);
+                if (temp != null) {
+                    Integer shorter = conn.distance;
+                    
+                    if (temp > 0) {
+                        shorter += temp;
+                    }
+                    
+                    
+                    if (shrt == null || shrt > shorter) {
+                        shrt = shorter;
+                        if (max == null || max > shrt) {
+                            max = shrt;
+                        }
+                        
+                    } 
+                }
+            }
+        }
+        path[sp--] = null;
+        //max = shrt;
+        return shrt;
+    }
+
+    
+    
+    
+    
+    
+    
+}
+
