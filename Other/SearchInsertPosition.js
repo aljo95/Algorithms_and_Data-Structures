53757e5aeb3e2b4e275363a3d240ae735ae78395
linux laptop <johansson_a95@hotmail.com>
2024-03-14 17:07:57 +0100

    Recurisve AND looping solutions

--- Other/SearchInsertPosition.js
+++ Other/SearchInsertPosition.js
@@ -3,20 +3,42 @@
  * @param {number} target
  * @return {number}
  */
-var searchInsert = function(nums, target) {
+var searchInsertRECURSIVE = function(nums, target) {
 
   let mid = Math.floor((nums.length)/2);
-  
-  if (nums[mid] == target) return mid;
-  if (mid <= 1 || nums.length == 1) return 1;
 
-  if (nums[mid] > target) return mid - searchInsert(nums.slice(0, mid), target);
-  else if (nums[mid] < target) return mid + searchInsert(nums.slice(mid), target);
-		// 1: return 2 + f([5,6], 7)
-		// 2: return 2 + 1 + shit
+  if (nums[mid] == target) return mid;
+  if (nums.length == 2 && target > nums[1]) return 2;
+  else if (mid <= 1) return 1;
+
+  if (nums[mid] > target) return mid - searchInsertRECURSIVE(nums.slice(0, mid), target);
+  else if (nums[mid] < target) return mid + searchInsertRECURSIVE(nums.slice(mid), target);
 };
 
-console.log(searchInsert([1, 3, 5, 6], 5)); 		// Return 2
-console.log(searchInsert([1, 3, 5, 6], 2)); 		// Return 1
-console.log(searchInsert([1, 3, 5, 6], 7)); 		// Return 4
-console.log(searchInsert([1, 3, 5, 6, 8], 7)); 		// Return 4
+console.log(searchInsertRECURSIVE([1, 3, 5, 6], 5)); 		// Return 2
+console.log(searchInsertRECURSIVE([1, 3, 5, 6], 2)); 		// Return 1
+console.log(searchInsertRECURSIVE([1, 3, 5, 6], 7)); 		// Return 4
+
+
+
+
+var searchInsertLOOP = function(nums, target) {
+
+  let hi = nums.length-1;
+  let lo = 0;
+  while (hi > lo) {   //  2 3 4 5 6 7   => mid is 4.5 => 7-2/2 + 2 = 4.5 => floor = 4 correct?
+
+    let mid = Math.floor(((hi-lo)/2)+lo); // will be 1 first
+
+    if (nums[mid] == target) return mid;
+    if (nums[mid] > target) hi = hi - 1;
+    if (nums[mid] < target) lo = lo + 1;
+
+  }
+  return lo;
+}
+
+console.log(searchInsertRECURSIVE([1, 3, 5, 6], 5));          // Return 2
+console.log(searchInsertRECURSIVE([1, 3, 5, 6], 2));          // Return 1
+console.log(searchInsertRECURSIVE([1, 3, 5, 6], 7));          // Return 4
+
